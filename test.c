f0d0aaf78183d7cc397e85295d5c9a1c63b54bb9
avaneeshd <avaneeshdesai@gmail.com>
2016-04-29 21:46:06 -0700

    updated test cases

--- test.c
+++ test.c
@@ -11,7 +11,7 @@ int main()
 	char * s = "Hello string12356";
 	char * d = malloc(sizeof(char)*15);
 	printf("%s %x\n"," destination address: ", d);
-	
+
 // TEST CASE  : gets
 
 	char * gets_ex = malloc(sizeof(char) * 10);
@@ -21,21 +21,20 @@ int main()
 
 
 
-
 // TEST CASE : strcpy
-	
-	// char *res = strcpy(d,s);
-	// if(res == NULL) 
-	// {
-	// 	printf("%s\n", "value returned NULL" );
-	// }
-	// else{
-	// 	printf("%s\n", res);		
-	// }
-	
+
+	char *res = strcpy(d,s);
+	if(res == NULL)
+	{
+		printf("%s\n", "value returned NULL" );
+	}
+	else{
+		printf("%s\n", res);
+	}
+
 
 // TEST CASE: malloc
-/*
+
 	printf("Hello World ! %s", "Abhijeet\n");
 
 	int* p = (int*) malloc(sizeof(int));
@@ -48,21 +47,18 @@ int main()
 
 	*p = 10;
 	printf("%s %d\n", "Value is :", *p);
-*/
+
 
 // TEST CASE : strcat
 
-// char * dest= "test";
-// d = strcpy(d, dest);
-// printf("%s \n" ,d);
-// char * dest2 = " longer long longer longest";
-// d = strcat(d , dest2);
-// printf("%s\n", d);
+	char * dest= "test";
+	d = strcpy(d, dest);
+	printf("%s \n" ,d);
+	char * dest2 = " longer long longer longest";
+	d = strcat(d , dest2);
+	printf("%s\n", d);
 
 free(d);
-
-
-
 
 	return 0;
 }
